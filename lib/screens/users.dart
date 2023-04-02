0c1eec737db4f1aa25563dea2448137e77b151dd
Luis Angel <luisparr14@gmail.com>
2023-04-02 15:19:21 -0500

    clase #62 - flutter avanzado

--- lib/screens/users.dart
+++ lib/screens/users.dart
@@ -1,6 +1,8 @@
-import 'package:chat_app/models/user.dart';
 import 'package:flutter/material.dart';
 
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import 'package:chat_app/models/user.dart';
 class UsersScreen extends StatefulWidget {
   const UsersScreen({super.key});
 
@@ -9,6 +11,9 @@ class UsersScreen extends StatefulWidget {
 }
 
 class _UsersScreenState extends State<UsersScreen> {
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
   final users = [
     User(name: 'Luis', email: 'test1@test,com', online: true, uid: '1'),
     User(name: 'Daniel', email: 'test2@test,com', online: false, uid: '2'),
@@ -34,25 +39,43 @@ class _UsersScreenState extends State<UsersScreen> {
                   Icon(Icons.offline_bolt_outlined, color: Colors.red[400]))
         ],
       ),
-      body: Center(
-          child: ListView.separated(
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (context, i) => ListTile(
-                  title: Text(users[i].name),
-                  leading: CircleAvatar(
-                    child: Text(users[i].name.toUpperCase().substring(0, 2)),
-                  ),
-                  trailing: Container(
-                    width: 10,
-                    height: 10,
-                    decoration: BoxDecoration(
-                        color: users[i].online
-                            ? Colors.green.shade500
-                            : Colors.red,
-                        borderRadius: BorderRadius.circular(100)),
-                  )),
-              separatorBuilder: (_, i) => const Divider(),
-              itemCount: users.length)),
+      body: SmartRefresher(
+        controller: _refreshController,
+        header: const WaterDropMaterialHeader(
+          distance: 25
+        ),
+        onRefresh: _onRefresh,
+        child: _usersListView()
+        ),
     );
+  }
+
+  ListView _usersListView() {
+    return ListView.separated(
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (context, i) => _usersListTile(users[i]),
+        separatorBuilder: (_, i) => const Divider(),
+        itemCount: users.length);
+  }
+
+  ListTile _usersListTile(User user) {
+    return ListTile(
+        title: Text(user.name),
+        leading: CircleAvatar(
+          child: Text(user.name.toUpperCase().substring(0, 2)),
+        ),
+        trailing: Container(
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+              color: user.online ? Colors.green.shade500 : Colors.red,
+              borderRadius: BorderRadius.circular(100)),
+        ));
+  }
+
+  void _onRefresh() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
