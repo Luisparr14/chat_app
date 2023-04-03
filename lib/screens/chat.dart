ff1d64044b26df2eb806b4a591c805b1849c0103
Luis Angel <luisparr14@gmail.com>
2023-04-02 22:11:20 -0500

    clase #65 - flutter avanzado

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,14 +1,83 @@
 import 'package:flutter/material.dart';
 
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
 
-class ChatScreen extends StatelessWidget {
-const ChatScreen({super.key});
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+  final _focusNode = FocusNode();
+  bool _isWriting = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ChatScreen')
-     ),
-   );
+      appBar: AppBar(
+        elevation: 1,
+        centerTitle: true,
+        title: const Column(
+          children: [
+            CircleAvatar(
+              maxRadius: 18,
+              child: Text('DA'),
+            ),
+            SizedBox(height: 2),
+            Text('Dante', style: TextStyle(fontSize: 12))
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+                child: ListView.builder(
+              itemBuilder: (_, i) => Text('$i'),
+              reverse: true,
+            )),
+            const Divider(height: 2),
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.all(10),
+              child: Row(children: [
+                Flexible(
+                    child: TextField(
+                  onSubmitted: _isWriting ? _handleSubmit : null,
+                  controller: _textController,
+                  decoration:
+                      const InputDecoration.collapsed(hintText: 'Send message'),
+                  focusNode: _focusNode,
+                  onChanged: (text) {
+                    if (text.trim().isNotEmpty) {
+                      _isWriting = true;
+                    } else {
+                      _isWriting = false;
+                    }
+                    setState(() {});
+                  },
+                )),
+                IconTheme(
+                  data: IconThemeData(color: Colors.blue.shade400),
+                  child: IconButton(
+                      onPressed: _isWriting
+                          ? () => _handleSubmit(_textController.text)
+                          : null,
+                      icon: const Icon(Icons.send)),
+                )
+              ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmit(String text) {
+    _textController.clear();
+    _focusNode.requestFocus();
+    _isWriting = false;
+    setState(() {});
   }
 }
