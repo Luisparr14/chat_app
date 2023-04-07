70b45f1bc8bc161cae5ab037adbb212d3ce7d859
Luis Angel <luisparr14@gmail.com>
2023-04-07 00:31:49 -0500

    clase #108 - flutter avanzado

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -1,5 +1,7 @@
 import 'package:chat_app/global/enviroment.dart';
+import 'package:chat_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 enum ServerStatus { online, offline, connecting }
@@ -11,12 +13,19 @@ class SocketService with ChangeNotifier {
   ServerStatus get serverStatus => _serverStatus;
   IO.Socket get socket => _socket;
 
-  void connect() {
+  void connect() async {
+
+    final token = await AuthService.getToken();
+
     _socket = IO.io(
         Enviroment.socketBaseUrl(),
         IO.OptionBuilder()
             .setTransports(['websocket'])
             .enableAutoConnect()
+            .enableForceNew()
+            .setExtraHeaders({
+              'x-token': token
+            })
             .build());
 
     _socket.onConnect((data) {
